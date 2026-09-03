3fbe974e558a29c44c23a57dcf569ed8e459b741
Bartu Ozcan <bartu.ozcan2004@gmail.com>
2026-09-03 18:20:06 +0300

    feat: Ros paketleri icin debian uretecek bloom komutlari eklendi

--- Tools/packageScript.sh
+++ Tools/packageScript.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+cd $(dirname "$0")/..
+
+for dir in src; do
+    if [ -d "$dir" ]; then
+        bloom-generate rosdebian --os-name ubuntu --os-version jammy --ros-distro jazzy
+
+        nocheck=1 fakeroot debian/rules binary
+    fi
+done
+
+mkdir -p package
+mv *.deb package/
+mv *.ddeb package/
